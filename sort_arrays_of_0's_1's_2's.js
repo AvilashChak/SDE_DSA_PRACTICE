7e8a4ca9ed6361fb9abba577cc919098d748f733
AvilashChak <avilashchak@gmail.com>
2025-02-22 23:03:23 +0530

    Sort an array of 0's, 1's and 2's

--- sort_arrays_of_0's_1's_2's.js
+++ sort_arrays_of_0's_1's_2's.js
@@ -23,7 +23,29 @@
 // So we can say that mid to high is the unsorted part of the array which we need to sort
 // Therefore we can say that the start point is mid and end is high. We can also state that the start is low and then we observe that a[mid] == 0 -> here we wil swap a[low],a[mid] and move forwards/increment low++ and mid++; a[mid] == 1 -> here we will move forwards mid++, a[mid] == 2 -> here we will swap a[mid],a[high] and shrink/decrement high--.
 
+
 // Solution
+// Better - O(n + n) and SC - O(1)
+
+const sortArraysBetter = function(arr) {
+    let cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === 0) cnt0++;
+        else if(arr[i] === 1) cnt1++;
+        else cnt2++;
+    }
+
+    // replace the places in the original arr
+    for(let i = 0; i < cnt0; i++) arr[i] = 0;
+    for(let i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1;
+    for(let i = cnt0 + cnt1; i < arr.length; i++) arr[i] = 2;
+
+    return arr;
+};
+
+// Solution
+// Optimal Solution - TC - O(N) and SC - O(1)
 const sortArrays = function(nums) {
     let low = 0, mid = 0, high = nums.length - 1;
     while(mid <= high) {
@@ -41,6 +63,12 @@ const sortArrays = function(nums) {
     return nums;
 };
 
+
+
+
+const arr1 = [2,0,2,1,1,0];
+console.log("Using better approach:", sortArraysBetter(arr1));
+console.log();
 const arr = [2,0,2,1,1,0];
 const result = sortArrays(arr);
 console.log(result);
