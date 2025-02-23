4986e5c2a6351c0e02f651cc561e047cee83f06b
AvilashChak <avilashchak@gmail.com>
2025-02-23 11:43:30 +0530

    merge sorted arrays

--- merge_sorted_arrays.js
+++ merge_sorted_arrays.js
@@ -28,7 +28,59 @@
 
 // Approach
 
+// BF - TC - O(m + n) and SC - O(m + n)
+
+const mergeBF = function(arr1, m, arr2, n) {
+    // Declare a 3rd arr and two pointers
+    let arr3 = new Array(m + n);
+    let left = 0, right = 0;
+
+    let index = 0;
+
+    // Insert the elements from the 2 arrays into the 3rd arr using left and right pointers
+    while(left < m && right < n) {
+        if(arr1[left] <= arr2[right]) {
+            arr3[index] = arr1[left];
+            left++;
+            index++;
+        }
+        else {
+            arr3[index] = arr2[right];
+            right++;
+            index++;
+        }
+    }
+
+    // if the left pointer reaches the end
+    while(left < m) {
+        arr3[index] = arr1[left];
+        left++;
+        index++;
+    }
+
+    // if the right pointer reaches the end
+    while(right < n) {
+        arr3[index] = arr2[right];
+        right++;
+        index++;
+    }
+
+    // fill back the elements from arr3 to arr1 and arr2
+    for(let i = 0; i < m + n; i++) {
+        arr1[i] = arr3[i];
+    }
+};
+
+let arr3 = [1,2,3,0,0,0];
+let m3 = 3; 
+let arr4 = [2,5,6];
+let n4 = 3;
+mergeBF(arr3, m3, arr4, n4);
+console.log("Using BF approach:", arr3);
+
+
 // Solution
+// Optimal - TC - O(m + n) and SC - O(1)
 const merge = function(nums1, m, nums2, n) {
     let p1 = m - 1;
     let p2 = n - 1;
@@ -56,7 +108,17 @@ let m = 3;
 let arr2 = [2,5,6];
 let n = 3;
 merge(arr1, m, arr2, n);
-//console.log(arr1);
+console.log("Using Optimal approach:",arr1);
+
+
+
+
+
+
+
+
+
+// Using gap method
 
 const swapIfGreater = function(arr1, arr2, ind1, ind2) {
     if(arr1[ind1] > arr2[ind2]) {
@@ -64,7 +126,6 @@ const swapIfGreater = function(arr1, arr2, ind1, ind2) {
     }
 }
 
-// Using gap method
 const mergeGap = function(arr1, m, arr2, n) {
     let len = (m + n);
     let gap = Math.ceil(len / 2);
@@ -99,10 +160,10 @@ const mergeGap = function(arr1, m, arr2, n) {
     }
 };
 
-const ar1 = [1, 3, 5, 7];
-const ar2 = [0, 2, 6, 8, 9];
-let m1 = 4;
-let n2 = 5;
-mergeGap(ar1, m1, ar2, n2);
-console.log("res:", JSON.stringify(ar1));
-console.log("resL:", ar1.length);
+// const ar1 = [1, 3, 5, 7];
+// const ar2 = [0, 2, 6, 8, 9];
+// let m1 = 4;
+// let n2 = 5;
+// mergeGap(ar1, m1, ar2, n2);
+// console.log("res:", JSON.stringify(ar1));
+// console.log("resL:", ar1.length);
